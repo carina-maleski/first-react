0afabeff9663648d2e7458e83b9890e4b7fbfb3d
carina-maleski <carinamaleski@gmail.com>
2024-02-19 17:21:57 -0300

    create AuthProvider wrap

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,25 @@ import NavBar from "./components/navBar/NavBar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./paginas/login/Login";
 import Cadastro from "./paginas/cadastro/Cadastro";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <NavBar />
-        <div className="min-h-[80vh]">
-          <Routes>
-            <Route path="/" element={<Cadastro />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/cadastro" element={<Cadastro />} />
-            <Route path="/home" element={<Home />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <NavBar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </>
   );
 }
