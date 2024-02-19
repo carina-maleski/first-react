c993bea8e05af30461240bd3fbf752a46bd0f483
carina-maleski <carinamaleski@gmail.com>
2024-02-19 19:00:12 -0300

    config image

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -7,17 +7,17 @@ function Home() {
       <div className="bg-cyan-900 flex justify-center">
         <div className="container grid grid-cols-2 text-stone-100">
           <div className="flex flex-col gap-4 items-center justify-center py-4">
-            <h2 className="text-5xl font-bold">Seja bem vinde!</h2>
+            <h2 className="text-5xl font-bold">Boas-vindas!</h2>
             <p className="text-xl">Expresse aqui seus pensamentos e opiniões</p>
             <div className="flex justify-around gap-4">
-              <button className="rounded bg-stone-100 text-cyan-800 py-2 px-4">
+              <button className="rounded bg-stone-100 text-cyan-800 py-2 px-4 font-semibold hover:bg-cyan-950 hover:text-stone-100">
                 Ver postagens
               </button>
             </div>
           </div>
 
           <div className="flex justify-center">
-            <img src={homeLogo} alt="" className="w-2/6" />
+            <img src={homeLogo} alt="cachorrinho" className="w-100" />
           </div>
         </div>
       </div>
