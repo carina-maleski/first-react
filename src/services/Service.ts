9d0943160065171d6c7eb9f54e3281e11c087d81
carina-maleski <carinamaleski@gmail.com>
2024-02-22 15:29:18 -0300

    correct url

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://blogpessoal-tsss.onrender.com/",
+  baseURL: "https://blogpessoal-tsss.onrender.com",
 });
 
 export const cadastrarUsuario = async (
