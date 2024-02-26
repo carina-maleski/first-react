7226ddc7acbf0b64149b3b4c826980db45556a96
carina-maleski <carinamaleski@gmail.com>
2024-02-26 16:08:58 -0300

    add FormularioPostagem route

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import { AuthProvider } from "./contexts/AuthContext";
 import ListaTemas from "./components/temas/listaTemas/ListaTemas";
 import FormularioTema from "./components/temas/formularioTema/FormularioTema";
 import DeletarTema from "./components/temas/deletarTema/DeletarTema";
+import ListaPostagens from "./components/postagens/listaPostagens/ListaPostagens";
+import FormularioPostagem from "./components/postagens/formularioPostagem/FormularioPostagem";
 
 function App() {
   return (
@@ -26,6 +28,19 @@ function App() {
               <Route path="/cadastroTema" element={<FormularioTema />} />
               <Route path="/editarTema/:id" element={<FormularioTema />} />
               <Route path="/deletarTema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
+              <Route
+                path="/cadastroPostagem"
+                element={<FormularioPostagem />}
+              />
+              <Route
+                path="/editarPostagem/:id"
+                element={<FormularioPostagem />}
+              />
+              {/* <Route
+                path="/deletarPostagem/:id"
+                element={<DeletarPostagem />}
+              /> */}
             </Routes>
           </div>
           <Footer />
