401b89e0384884a0dc3fb35768d80e4c998d085f
carina-maleski <carinamaleski@gmail.com>
2024-02-27 13:43:33 -0300

    add toastAlert

--- src/components/postagens/formularioPostagem/FormularioPostagem.tsx
+++ src/components/postagens/formularioPostagem/FormularioPostagem.tsx
@@ -5,6 +5,7 @@ import { AuthContext } from "../../../contexts/AuthContext";
 import { atualizar, buscar, cadastrar } from "../../../services/Service";
 import Tema from "../../../models/Tema";
 import Postagem from "../../../models/Postagem";
+import { toastAlerta } from "../../../util/toastAlerta";
 
 function FormularioPostagem() {
   const navigate = useNavigate();
@@ -56,7 +57,7 @@ function FormularioPostagem() {
 
   useEffect(() => {
     if (token === "") {
-      alert("Você precisa estar logado.");
+      toastAlerta("Você precisa estar logado.", "info");
       navigate("/");
     }
   }, [token]);
@@ -101,14 +102,14 @@ function FormularioPostagem() {
             Authorization: token,
           },
         });
-        alert("Postagem atualizada com sucesso!");
+        toastAlerta("Postagem atualizada com sucesso!", "sucesso");
         retornar();
       } catch (error: any) {
         if (error.toString().includes("403")) {
-          alert("O token expirou. Por favor, logar novamente.");
+          toastAlerta("O token expirou. Por favor, logar novamente.", "info");
           handleLogout();
         } else {
-          alert("Erro ao atualizar a Postagem.");
+          toastAlerta("Erro ao atualizar a Postagem.", "erro");
         }
       }
     } else {
@@ -119,14 +120,14 @@ function FormularioPostagem() {
           },
         });
 
-        alert("Postagem cadastrada com sucesso!");
+        toastAlerta("Postagem cadastrada com sucesso!", "sucesso");
         retornar();
       } catch (error: any) {
         if (error.toString().includes("403")) {
-          alert("O token expirou. Por favor, logar novamente.");
+          toastAlerta("O token expirou. Por favor, logar novamente.", "info");
           handleLogout();
         } else {
-          alert("Erro ao cadastrar a Postagem");
+          toastAlerta("Erro ao cadastrar a Postagem", "erro");
         }
       }
     }
