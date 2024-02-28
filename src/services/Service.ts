e461fdb908d05817eb99b39e298e42030e7b6f2c
carina-maleski <carinamaleski@gmail.com>
2024-02-28 09:40:46 -0300

    Deploy do Projeto Pessoal

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://blogpessoal-tsss.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const cadastrarUsuario = async (
